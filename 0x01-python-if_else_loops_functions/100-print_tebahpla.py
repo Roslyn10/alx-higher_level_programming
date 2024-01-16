d8786b25978077a61f181af5f8cbd740bdaf39f7
Roslyn10 <roslynclewis1004@gmail.com>
2024-01-16 23:10:43 +0200

    Adding 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for index, char_code in enumerate(reversed(range(ord('a'), ord('z') + 1))):
+    char = chr(char_code)
+    print(char.upper() if index % 2 != 0 else char, end="")
