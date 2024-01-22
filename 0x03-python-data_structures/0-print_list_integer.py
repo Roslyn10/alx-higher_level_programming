7e3a4234d91dcb762f2c53783fe4130fdd2d0cdb
Roslyn10 <roslynclewis1004@gmail.com>
2024-01-22 15:26:32 +0200

    Adding int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     for element in my_list:
         if isinstance(element, int):
-            print("{}".format(element))
+            print("{:d}".format(element))
