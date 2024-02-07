457d13f40be3c6572f07f6b66c2638de294bdae2
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-07 15:50:27 +0200

    Adding code for file 3 and making the test file executable

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    try:
+        total = a / b
+    except ZeroDivisionError:
+        total = None
+    finally:
+        print("Inside result: {}".format(total))
+    return total
+
