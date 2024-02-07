e39235eb4146694c25caaf59527084b1a55dfc38
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-07 23:19:11 +0200

    Adding code for file 1, and checking pycodestyle and indentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""A classs Square that defines a square"""
+
+
+class Square:
+
+    """Size of the square"""
+    def __init__(self, size):
+
+        """
+        Initializes square with the given size.
+
+        size (int): Length of each side of square
+        """
+        self.__size = size
