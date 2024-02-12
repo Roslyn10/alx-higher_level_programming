325e782fc27d31057877cecafd8518ea15d7b801
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-12 11:18:12 +0200

    Adding code for file 0 which is a function that adds 2 integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a function that adds integers"""
+
+
+def add_integer(a=56, b=98):
+    """Returns the addition of two numbers
+    raises:
+        TypeError if a and b is not float or int 
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer") 
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b mus be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b) 
+    return a + b 
