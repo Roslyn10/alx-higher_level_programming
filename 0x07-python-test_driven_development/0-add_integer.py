9d9cd2cf51f65ba7f9892e8612f60ba3f0e6eca1
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-13 19:02:08 +0200

    Fixing typo with typeerror, from mus to must

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,13 +4,16 @@
 
 def add_integer(a=56, b=98):
     """Returns the addition of two numbers
+
+    Arguments:
+        a: 
     raises:
         TypeError if a and b is not float or int 
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer") 
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b mus be an integer")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
