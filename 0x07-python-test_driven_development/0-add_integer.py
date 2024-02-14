0570064a54da40628d32540475b9952645ae3578
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-14 12:50:33 +0200

    fixing pycodestyle for 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,16 +3,16 @@
 
 
 def add_integer(a=56, b=98):
-    """Returns the addition of two number 
+    """Returns the addition of two number
     raises:
-        TypeError if a and b is not float or int 
+        TypeError if a and b is not float or int
     """
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer") 
+        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
-        b = int(b) 
-    return a + b 
+        b = int(b)
+    return a + b
