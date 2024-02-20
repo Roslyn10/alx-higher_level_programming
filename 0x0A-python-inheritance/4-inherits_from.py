b0282c25011b08bdb8d842a42007285d61880475
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-20 20:42:23 +0200

    Adding code and test cases for file 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a function that checks the class and inheritance of an object"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited from a specified class, otherwise False
+
+    Arguments:
+        obj: An object that is a specific class
+        a_class: The class to match the object to
+
+    Returns:
+        True: if the object matches the class
+        False: If otherwise 
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
