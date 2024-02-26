9919318fa25044cf6ab51cecda90ae70f144ad36
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-26 20:41:40 +0200

    Adding dictionary representation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -47,3 +47,13 @@ class Square(Rectangle):
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a square"""
+
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
