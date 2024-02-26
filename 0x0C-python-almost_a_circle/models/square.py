7a0716de40fe3d4a9da3de11da56b0336bddd413
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-27 01:32:11 +0200

    Fixing pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """A Square class that inherits from rectangle"""
 
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Square class"""
-
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a Square instance"""
         super().__init__(size, size, x, y, id)
@@ -35,22 +35,21 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute"""
-            if args:
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.size = args[1]
-        if len(args) > 2:
-            self.x = args[2]
-        if len(args) > 3:
-            self.y = args[3]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
 
-    for key, value in kwargs.items():
-        setattr(self, key, value)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def to_dictionary(self):
         """Returns the dictionary representation of a square"""
-
         return {
             "id": self.id,
             "size": self.size,
