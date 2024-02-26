598b91d43c4a2a2a15ed0b6b3e27d2d2775380b3
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-26 12:29:46 +0200

    Adding additional code for raising errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,6 +19,28 @@ class Rectangle(Base):
             y: Y-coordinate of the rectangle.
             id: The identifier of the rectangle.
         """
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.width = width
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.height = height
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.x = x
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.y = y
+
         super().__init__(id)
         self.width = width
         self.height = height
