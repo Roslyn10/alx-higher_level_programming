cebb30277d04caff79a820fd297feed98d480948
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-26 11:54:51 +0200

    Adding the code for rectangle base, getter/setter

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""A rectangle class that inherits from Base"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width: The width of the rectangle.
+            height: The height of the rectangle.
+            x: X-coordinate of the rectangle.
+            y: Y-coordinate of the rectangle.
+            id: The identifier of the rectangle.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.__y = value
