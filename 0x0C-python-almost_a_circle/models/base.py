283334558878ce1405e1bbc89ff9dbe47f5b7f81
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-26 21:56:30 +0200

    Adding JSON string represention to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
 """Base class module"""
+import json
+import os
 
 
 class Base:
-    """Base classs representing unique objects"""
+    """Base class representing unique objects"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Inititalize an object with a uniquie id
+        """Initialize an object with a unique id
 
-        Arguments:
+        Args:
             id (int): The identifier for the object.
-
         """
-
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries
+
+        Returns:
+            str: JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
