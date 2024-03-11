df4e71f4bbf842a3dab1f80909f2e818c19fb7bc
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-11 16:07:36 +0200

    Fixing code output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j), end="" if j == 9 else "")
-print("\n")
+for r in range(9):
+    for l in range(r + 1, 10):
+        if r * 10 + l < 89:
+            print("{:d}{:d}".format(r, l), end=", ")
+print("{:d}".format(89))
