6fb67d9be0fa2966d7fa311bd1bafa2557c1d177
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-11 16:28:15 +0200

    Fixing import error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calcualation_102.sub
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
