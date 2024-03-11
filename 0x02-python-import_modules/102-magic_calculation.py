302a739fa718e8303bcae53eb74f879233329e44
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-11 16:25:29 +0200

    Adding magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def magic_calculation(a, b):
+    add = magic_calculation_102.add
+    sub = magic_calcualation_102.sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
