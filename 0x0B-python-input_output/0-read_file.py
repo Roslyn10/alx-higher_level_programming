7e1a3aeb474c425a1f125294e432af77f17521a8
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-19 15:17:31 +0200

    Fixing pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that reads text files"""
 
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout"""
     with open(filename, encoding='utf-8') as file:
