5b84f72ee0c45a2b73d86a7e367551556248c65e
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-26 16:13:28 +0200

    updating the README.md

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that adds all arguments to a Python list"""
+
+
+import json
+
+
+
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function that returns the dictionary description"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """Returns the dic description with simple data structure for JSON"""
+
+
