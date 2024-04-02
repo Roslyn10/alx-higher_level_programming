49a86b50a988f65960781080690f36dc817b259c
Roslyn10 <roslynclewis1004@gmail.com>
2024-04-02 15:54:45 +0200

    Removing the extra score and name command

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,6 +1,5 @@
 -- Removes all records with a score <= 5
 -- in the second_table in hbtn_0c_0 
 -- mysql will be passed as an argument
-DELETE score, name 
-FROM second_table
+DELETE FROM second_table
 WHERE score <5;
