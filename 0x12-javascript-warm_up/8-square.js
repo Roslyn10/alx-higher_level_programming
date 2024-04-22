c79ada00c42c6e28fff0a48903a6d2e1eee39781
Roslyn10 <roslynclewis1004@gmail.com>
2024-04-22 21:54:56 +0200

    Adding the parameter for strings and non integers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,6 +6,9 @@ if (process.argv.length !== 3) {
 
 const size = parseInt(process.argv[2]);
 
+if (isNaN(size)) { 
+	console.log('Missing size');
+}
 for (let i = 0; i < size; i++) {
   let row = '';
   for (let j = 0; j < size; j++) {
