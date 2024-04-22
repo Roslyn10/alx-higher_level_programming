6e106cd7f042715dbd0b851168d114441c809f5d
Roslyn10 <roslynclewis1004@gmail.com>
2024-04-22 15:35:05 +0200

    Removing var

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const myvar of ['C is fun\nPython is cool\nJavaScript is amazing']) {
-  console.log(myvar);
+for (const myline of ['C is fun\nPython is cool\nJavaScript is amazing']) {
+  console.log(myline);
 }
