5eb40297448584616270a42caa5065fd0b9f657a
Roslyn10 <roslynclewis1004@gmail.com>
2024-04-22 21:02:03 +0200

    Adding 5. An integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv[2] !== undefined) {
+  const argAsInt = parseInt(process.argv[2]);
+  if (!isNaN(argAsInt)) {
+    console.log('My number:', argAsInt);
+  } else {
+    console.log('Not a number');
+  }
+} else {
+	console.log('Not a number');
+}
