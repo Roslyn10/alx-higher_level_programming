01a976348c6ea0cafafed40be2e4c141d2252e12
Roslyn10 <roslynclewis1004@gmail.com>
2024-04-30 12:08:49 +0200

    Trying to fix the character count

--- 0x04-python-more_data_structures/6-main.py
+++ 0x04-python-more_data_structures/6-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = {‘languages’: “C”, ‘number’: 13, ‘track’: “Low level” }
+a_dictionary = {'languages': "C", 'number': 13, 'track': "Low level" }
 print_sorted_dictionary(a_dictionary)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
+    sorted_keys = sorted(a_dictionary)
     for key in sorted_keys:
         print(key, ":", a_dictionary[key])
