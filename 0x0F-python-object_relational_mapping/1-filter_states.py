026834e716b485374e994274da97a5cabf4a5695
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-02 12:44:40 +0200

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    Lists all states with a name starting with N from 
+    Lists all states with a name starting with N from
     the datatbase hbtn_0e_0_usa
     Arguments:
         mysql username
@@ -21,7 +21,12 @@ if __name__ == '__main__':
 
     cursor = conn.cursor()
 
-    query = "SELECT * FROM states WHERE state_name LIKE 'N%' ORDER BY states.id ASC"
+    query = """
+            SELECT *
+            FROM states
+            WHERE state_name LIKE 'N%'
+            ORDER BY states.id ASC
+            """
     cursor.execute(query)
 
     states = cursor.fetchall()
@@ -30,4 +35,4 @@ if __name__ == '__main__':
         print(state)
 
     cursor.close()
-    db.close()
+    db.st close()
