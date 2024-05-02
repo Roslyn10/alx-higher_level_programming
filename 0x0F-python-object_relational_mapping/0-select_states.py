75c9866b93e183f37cf7bf61dcaca6e44007c5cc
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-02 10:47:41 +0200

    Adding the code for 0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+
+"""
+    Lists all states from the datatbase hbtn_0e_0_usa
+    Arguments:
+        mysql username
+        mysql password
+        database name
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == 'main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursos = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
