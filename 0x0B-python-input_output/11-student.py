7310f854b098cbb57f7eee3dd936da08649c2b5e
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-03 13:07:41 +0200

    Adding the code for 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class; Student; that defines a student"""
 
+
 class Student:
     """
     Attributes
@@ -8,12 +9,29 @@ class Student:
     last_name(str): last name of the student
     age(int): age of the student
     """
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """returns the dictionary representation of the student class"""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """returns the dictionary representation of the student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                    key: value for key,
+                    value in self.__dict__.items() if key in attrs
+                    }
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+
+        Attributes:
+            json (dict): Key that will replace the attributes with
+
+        """
+
+        for j, g in json.items():
+            setattr(self, j, g)
