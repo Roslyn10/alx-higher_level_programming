257a67c2d81e9eac3f2253f7202eefd96bea77ee
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-12 21:29:39 +0200

    Trying to fix it

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,5 @@
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    let moviesList = data.results; // Assuming the data structure has a 'results' array
+    let movies = moviesList.map(movie => movie.title).join(', '); // Combine titles into a single string
+    $('DIV#list_movies').text(movies);
 }); 
