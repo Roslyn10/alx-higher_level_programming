911baeb8e95498f40e4ca4082e05b1c153d0de79
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-14 14:09:35 +0200

    Adding quotation marks

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in a URL as an arguemnet, sends a GET request to the URl, and displays the body of the response.
-curl -s "$1" -H X-School-User-Id: 98
+curl -s "$1" -H "X-School-User-Id: 98"
