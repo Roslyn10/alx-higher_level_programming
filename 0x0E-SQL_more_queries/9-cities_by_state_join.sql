572220bb917c340c684d35656b1d1fc0318a1f76
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-21 12:42:58 +0200

    Fixing the code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities contained in the database hbtn_0d_usa
 -- Should be displayed in ascending order by city.id
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+SELECT cities.id, city.name, states.name
 FROM citites
-LEFT JOIN states ON states.id = cities.state_id
-ORDER BY cities.id;
+INNER JOIN states
+ON cities.sate_id = states.id;
