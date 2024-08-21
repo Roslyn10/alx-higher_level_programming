ca54dae55dd6b28b0471370d7c8b55d446d20de4
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-21 12:15:47 +0200

    Fixing somemore spelling mistakes

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,6 @@
 -- Displays the average temperature by city
 -- ordered by temperature
 
-USE temperature;
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 -- ordered by temp (dec)
 
 SELECT city, AVG(value) as avg_temp
-FROM tempreatures
+FROM temperatures
 WHERE month = 7 or month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
