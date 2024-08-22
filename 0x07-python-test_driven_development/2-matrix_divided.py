8c27680d01d5b3d4f36ed49727e289dc8a652e02
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-22 21:27:59 +0200

    Fixing pycode errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,36 +7,38 @@ def matrix_divided(matrix, div):
     Divides all elements of a matrix
 
     Arguments:
-        matrix(int/float): list of lists
-        div(int/float): element that the matrices are divided by
+        matrix (list of lists of int/float): the matrix to be divided
+        div (int/float): the divisor
 
     Raises:
-        TypeError: if the list is not ints or floats
-        TypeError: if the matrices are not the same size
+        TypeError: if the matrix is not a list of lists of integers/floats
+        TypeError: if the rows of the matrix are not the same size
         TypeError: if div is not an int or float
         ZeroDivisionError: if div is equal to 0
     """
     if type(matrix) is not list:
         raise TypeError(
-        "matrix must be a matrix (list of lists) of integers/floats"
+            "matrix must be a matrix (list of lists) of integers/floats"
         )
 
     size = None
     for s in matrix:
         if type(s) is not list:
             raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
+                "matrix must be a matrix (list of lists) of \
+                        integers/floats"
             )
         if size is None:
             size = len(s)
         elif size != len(s):
             raise TypeError(
-            "Each row of the matrix must have the same size"
+                "Each row of the matrix must have the same size"
             )
         for p in s:
             if type(p) is not int and type(p) is not float:
                 raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
+                    "matrix must be a matrix (list of lists) of \
+                            integers/floats"
                 )
 
     if type(div) is not int and type(div) is not float:
